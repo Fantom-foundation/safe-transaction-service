5968aef83b773298b80d19e9669ab8315fdf8d27
jason <kwon_jason@yahoo.com>
2021-02-27 05:53:38 +0300

    syning with opera testnet

--- safe_transaction_service/history/management/commands/setup_service.py
+++ safe_transaction_service/history/management/commands/setup_service.py
@@ -46,82 +46,84 @@ TASKS = [
 ]
 
 MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
+    # changing mastercopy contract addresses, all to opera testnet deployed contracts
     EthereumNetwork.MAINNET: [
-        ('0x6851D6fDFAfD08c0295C392436245E5bc78B0185', 10329734, '1.2.0'),
-        ('0x34CfAC646f301356fAa8B21e94227e3583Fe3F5F', 9084503, '1.1.1'),
-        ('0xaE32496491b53841efb51829d6f886387708F99B', 8915728, '1.1.0'),
-        ('0xb6029EA3B2c51D09a50B53CA8012FeEB05bDa35A', 7457553, '1.0.0'),
-        ('0x8942595A2dC5181Df0465AF0D7be08c8f23C93af', 6766257, '0.1.0'),
-        ('0xAC6072986E985aaBE7804695EC2d8970Cf7541A2', 6569433, '0.0.2'),
+        ('0xe3d4Af0e46Aa207222bC6FC11652dDF68eBF1d15', 367377, '1.2.0'),
+        ('0xe3d4Af0e46Aa207222bC6FC11652dDF68eBF1d15', 367377, '1.1.1'),
+        ('0xe3d4Af0e46Aa207222bC6FC11652dDF68eBF1d15', 367377, '1.1.0'),
+        ('0xe3d4Af0e46Aa207222bC6FC11652dDF68eBF1d15', 367377, '1.0.0'),
+        ('0xe3d4Af0e46Aa207222bC6FC11652dDF68eBF1d15', 367377, '0.1.0'),
+        ('0xe3d4Af0e46Aa207222bC6FC11652dDF68eBF1d15', 367377, '0.0.2'),
     ],
-    EthereumNetwork.RINKEBY: [
-        ('0x6851D6fDFAfD08c0295C392436245E5bc78B0185', 6723632, '1.2.0'),
-        ('0x34CfAC646f301356fAa8B21e94227e3583Fe3F5F', 5590754, '1.1.1'),
-        ('0xaE32496491b53841efb51829d6f886387708F99B', 5423491, '1.1.0'),
-        ('0xb6029EA3B2c51D09a50B53CA8012FeEB05bDa35A', 4110083, '1.0.0'),
-        ('0x8942595A2dC5181Df0465AF0D7be08c8f23C93af', 3392692, '0.1.0'),
-        ('0x2727D69C0BD14B1dDd28371B8D97e808aDc1C2f7', 3055781, '0.0.2'),
-    ],
-    EthereumNetwork.GOERLI: [
-        ('0x6851D6fDFAfD08c0295C392436245E5bc78B0185', 2930373, '1.2.0'),
-        ('0x34CfAC646f301356fAa8B21e94227e3583Fe3F5F', 1798663, '1.1.1'),
-        ('0xaE32496491b53841efb51829d6f886387708F99B', 1631488, '1.1.0'),
-        ('0xb6029EA3B2c51D09a50B53CA8012FeEB05bDa35A', 319108, '1.0.0'),
-        ('0x8942595A2dC5181Df0465AF0D7be08c8f23C93af', 34096, '0.1.0'),
-    ],
-    EthereumNetwork.KOVAN: [
-        ('0x6851D6fDFAfD08c0295C392436245E5bc78B0185', 19242615, '1.2.0'),
-        ('0x34CfAC646f301356fAa8B21e94227e3583Fe3F5F', 15366145, '1.1.1'),
-        ('0xaE32496491b53841efb51829d6f886387708F99B', 14740724, '1.1.0'),
-        ('0xb6029EA3B2c51D09a50B53CA8012FeEB05bDa35A', 10638132, '1.0.0'),
-        ('0x8942595A2dC5181Df0465AF0D7be08c8f23C93af', 9465686, '0.1.0'),
-    ],
-    EthereumNetwork.XDAI: [
-        ('0x6851D6fDFAfD08c0295C392436245E5bc78B0185', 10612049, '1.2.0'),
-        ('0x34CfAC646f301356fAa8B21e94227e3583Fe3F5F', 10045292, '1.1.1'),
-        ('0x2CB0ebc503dE87CFD8f0eCEED8197bF7850184ae', 12529466, '1.1.1-Circles'),
-    ],
-    EthereumNetwork.ENERGY_WEB_CHAIN: [
-        ('0x6851D6fDFAfD08c0295C392436245E5bc78B0185', 6398655, '1.2.0'),
-        ('0x34CfAC646f301356fAa8B21e94227e3583Fe3F5F', 6399212, '1.1.1'),
-    ],
-    EthereumNetwork.VOLTA: [
-        ('0x6851D6fDFAfD08c0295C392436245E5bc78B0185', 6876086, '1.2.0'),
-        ('0x34CfAC646f301356fAa8B21e94227e3583Fe3F5F', 6876642, '1.1.1'),
-    ]
+    # EthereumNetwork.RINKEBY: [
+    #     ('0xe3d4Af0e46Aa207222bC6FC11652dDF68eBF1d15', 367377, '1.2.0'),
+    #     ('0xe3d4Af0e46Aa207222bC6FC11652dDF68eBF1d15', 367377, '1.1.1'),
+    #     ('0xe3d4Af0e46Aa207222bC6FC11652dDF68eBF1d15', 367377, '1.1.0'),
+    #     ('0xe3d4Af0e46Aa207222bC6FC11652dDF68eBF1d15', 367377, '1.0.0'),
+    #     ('0xe3d4Af0e46Aa207222bC6FC11652dDF68eBF1d15', 367377, '0.1.0'),
+    #     ('0xe3d4Af0e46Aa207222bC6FC11652dDF68eBF1d15', 367377, '0.0.2'),
+    # ],
+    # EthereumNetwork.GOERLI: [
+    #     ('0x6851D6fDFAfD08c0295C392436245E5bc78B0185', 2930373, '1.2.0'),
+    #     ('0x34CfAC646f301356fAa8B21e94227e3583Fe3F5F', 1798663, '1.1.1'),
+    #     ('0xaE32496491b53841efb51829d6f886387708F99B', 1631488, '1.1.0'),
+    #     ('0xb6029EA3B2c51D09a50B53CA8012FeEB05bDa35A', 319108, '1.0.0'),
+    #     ('0x8942595A2dC5181Df0465AF0D7be08c8f23C93af', 34096, '0.1.0'),
+    # ],
+    # EthereumNetwork.KOVAN: [
+    #     ('0x6851D6fDFAfD08c0295C392436245E5bc78B0185', 19242615, '1.2.0'),
+    #     ('0x34CfAC646f301356fAa8B21e94227e3583Fe3F5F', 15366145, '1.1.1'),
+    #     ('0xaE32496491b53841efb51829d6f886387708F99B', 14740724, '1.1.0'),
+    #     ('0xb6029EA3B2c51D09a50B53CA8012FeEB05bDa35A', 10638132, '1.0.0'),
+    #     ('0x8942595A2dC5181Df0465AF0D7be08c8f23C93af', 9465686, '0.1.0'),
+    # ],
+    # EthereumNetwork.XDAI: [
+    #     ('0x6851D6fDFAfD08c0295C392436245E5bc78B0185', 10612049, '1.2.0'),
+    #     ('0x34CfAC646f301356fAa8B21e94227e3583Fe3F5F', 10045292, '1.1.1'),
+    #     ('0x2CB0ebc503dE87CFD8f0eCEED8197bF7850184ae', 12529466, '1.1.1-Circles'),
+    # ],
+    # EthereumNetwork.ENERGY_WEB_CHAIN: [
+    #     ('0x6851D6fDFAfD08c0295C392436245E5bc78B0185', 6398655, '1.2.0'),
+    #     ('0x34CfAC646f301356fAa8B21e94227e3583Fe3F5F', 6399212, '1.1.1'),
+    # ],
+    # EthereumNetwork.VOLTA: [
+    #     ('0x6851D6fDFAfD08c0295C392436245E5bc78B0185', 6876086, '1.2.0'),
+    #     ('0x34CfAC646f301356fAa8B21e94227e3583Fe3F5F', 6876642, '1.1.1'),
+    # ]
 
 }
 
 PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
+    # changing proxyfactory contract address, all -> opera testnet deployed contract' address
     EthereumNetwork.MAINNET: [
-        ('0x76E2cFc1F5Fa8F6a5b3fC4c8F4788F0116861F9B', 9084508),  # v1.1.1
-        ('0x50e55Af101C777bA7A1d560a774A82eF002ced9F', 8915731),  # v1.1.0
-        ('0x12302fE9c02ff50939BaAaaf415fc226C078613C', 7450116),  # v1.0.0
+        ('0x5eAa96A55e703BD410DCdc300683a05951080218', 369855),  # v1.1.1
+        ('0x5eAa96A55e703BD410DCdc300683a05951080218', 369855),  # v1.1.0
+        ('0x5eAa96A55e703BD410DCdc300683a05951080218', 369855),  # v1.0.0
     ],
-    EthereumNetwork.RINKEBY: [
-        ('0x76E2cFc1F5Fa8F6a5b3fC4c8F4788F0116861F9B', 5590757),
-        ('0x50e55Af101C777bA7A1d560a774A82eF002ced9F', 5423494),
-        ('0x12302fE9c02ff50939BaAaaf415fc226C078613C', 4110083),
-    ],
-    EthereumNetwork.GOERLI: [
-        ('0x76E2cFc1F5Fa8F6a5b3fC4c8F4788F0116861F9B', 1798666),
-        ('0x50e55Af101C777bA7A1d560a774A82eF002ced9F', 1631491),
-        ('0x12302fE9c02ff50939BaAaaf415fc226C078613C', 312509),
-    ],
-    EthereumNetwork.KOVAN: [
-        ('0x76E2cFc1F5Fa8F6a5b3fC4c8F4788F0116861F9B', 15366151),
-        ('0x50e55Af101C777bA7A1d560a774A82eF002ced9F', 14740731),
-        ('0x12302fE9c02ff50939BaAaaf415fc226C078613C', 10629898),
-    ],
-    EthereumNetwork.XDAI: [
-        ('0x76E2cFc1F5Fa8F6a5b3fC4c8F4788F0116861F9B', 10045327),
-    ],
-    EthereumNetwork.ENERGY_WEB_CHAIN: [
-        ('0x76E2cFc1F5Fa8F6a5b3fC4c8F4788F0116861F9B', 6399239),
-    ],
-    EthereumNetwork.VOLTA: [
-        ('0x76E2cFc1F5Fa8F6a5b3fC4c8F4788F0116861F9B', 6876681),
-    ]
+    # EthereumNetwork.RINKEBY: [
+    #     ('0x5eAa96A55e703BD410DCdc300683a05951080218', 369855),
+    #     ('0x5eAa96A55e703BD410DCdc300683a05951080218', 369855),
+    #     ('0x5eAa96A55e703BD410DCdc300683a05951080218', 369855),
+    # ],
+    # EthereumNetwork.GOERLI: [
+    #     ('0x76E2cFc1F5Fa8F6a5b3fC4c8F4788F0116861F9B', 1798666),
+    #     ('0x50e55Af101C777bA7A1d560a774A82eF002ced9F', 1631491),
+    #     ('0x12302fE9c02ff50939BaAaaf415fc226C078613C', 312509),
+    # ],
+    # EthereumNetwork.KOVAN: [
+    #     ('0x76E2cFc1F5Fa8F6a5b3fC4c8F4788F0116861F9B', 15366151),
+    #     ('0x50e55Af101C777bA7A1d560a774A82eF002ced9F', 14740731),
+    #     ('0x12302fE9c02ff50939BaAaaf415fc226C078613C', 10629898),
+    # ],
+    # EthereumNetwork.XDAI: [
+    #     ('0x76E2cFc1F5Fa8F6a5b3fC4c8F4788F0116861F9B', 10045327),
+    # ],
+    # EthereumNetwork.ENERGY_WEB_CHAIN: [
+    #     ('0x76E2cFc1F5Fa8F6a5b3fC4c8F4788F0116861F9B', 6399239),
+    # ],
+    # EthereumNetwork.VOLTA: [
+    #     ('0x76E2cFc1F5Fa8F6a5b3fC4c8F4788F0116861F9B', 6876681),
+    # ]
 }
 
 
@@ -129,7 +131,18 @@ class Command(BaseCommand):
     help = 'Setup Transaction Service Required Tasks'
 
     def handle(self, *args, **options):
-        for task in TASKS:
+        # for task in TASKS:
+        #     _, created = task.create_task()
+        #     if created:
+        #         self.stdout.write(self.style.SUCCESS('Created Periodic Task %s' % task.name))
+        #     else:
+        #         self.stdout.write(self.style.SUCCESS('Task %s was already created' % task.name))
+
+        # self.stdout.write(self.style.SUCCESS('Setting up Safe Contract Addresses'))
+        
+        # change to set up for opera test network
+        
+            for task in self.tasks:
             _, created = task.create_task()
             if created:
                 self.stdout.write(self.style.SUCCESS('Created Periodic Task %s' % task.name))
@@ -137,17 +150,44 @@ class Command(BaseCommand):
                 self.stdout.write(self.style.SUCCESS('Task %s was already created' % task.name))
 
         self.stdout.write(self.style.SUCCESS('Setting up Safe Contract Addresses'))
+        self.setup_my_network()
+        
         ethereum_client = EthereumClientProvider()
-        ethereum_network = ethereum_client.get_network()
-        if ethereum_network in MASTER_COPIES:
-            self.stdout.write(self.style.SUCCESS(f'Setting up {ethereum_network.name} safe addresses'))
-            self._setup_safe_master_copies(MASTER_COPIES[ethereum_network])
-        if ethereum_network in PROXY_FACTORIES:
-            self.stdout.write(self.style.SUCCESS(f'Setting up {ethereum_network.name} proxy factory addresses'))
-            self._setup_safe_proxy_factories(PROXY_FACTORIES[ethereum_network])
+        # ethereum_network = ethereum_client.get_network() -- this is for ethereum blockchain
+        # if ethereum_network in MASTER_COPIES:
+        #     self.stdout.write(self.style.SUCCESS(f'Setting up {ethereum_network.name} safe addresses'))
+        #     self._setup_safe_master_copies(MASTER_COPIES[ethereum_network])
+        # if ethereum_network in PROXY_FACTORIES:
+        #     self.stdout.write(self.style.SUCCESS(f'Setting up {ethereum_network.name} proxy factory addresses'))
+        #     self._setup_safe_proxy_factories(PROXY_FACTORIES[ethereum_network])
 
-        if not (ethereum_network in MASTER_COPIES and ethereum_network in PROXY_FACTORIES):
-            self.stdout.write(self.style.WARNING('Cannot detect a valid ethereum-network'))
+        # if not (ethereum_network in MASTER_COPIES and ethereum_network in PROXY_FACTORIES):
+        #     self.stdout.write(self.style.WARNING('Cannot detect a valid ethereum-network'))
+        
+        # changing for opera test network
+        # ethereum_network_id = ethereum_client.w3.net.version
+        # if ethereum_network in MASTER_COPIES:
+        #     self.stdout.write(self.style.SUCCESS(f'Setting up {ethereum_network.name} safe addresses'))
+        #     self._setup_safe_master_copies(MASTER_COPIES[ethereum_network])
+        # if ethereum_network in PROXY_FACTORIES:
+        #     self.stdout.write(self.style.SUCCESS(f'Setting up {ethereum_network.name} proxy factory addresses'))
+        #     self._setup_safe_proxy_factories(PROXY_FACTORIES[ethereum_network])
+
+        # if not (ethereum_network in MASTER_COPIES and ethereum_network in PROXY_FACTORIES):
+        #     self.stdout.write(self.style.WARNING('Cannot detect a valid ethereum-network'))
+        
+        ethereum_network_id = ethereum_client.w3.net.version
+        try:
+            self.stdout.write(self.style.SUCCESS(f'Setting up {4002} safe addresses'))
+            self._setup_safe_master_copies(ethereum_network_id)
+        except:
+            self.stdout.write(self.style.WARNING('Error in MASTER COPIES CREATION'))
+            
+        try:
+            self.stdout.write(self.style.SUCCESS(f'Setting up {ethereum_network.name} proxy factory addresses'))
+            self._setup_safe_proxy_factories(ethereum_network_id)
+        except:
+            self.stdout.write(self.style.WARNING('Error in PROXY FACTORIES CREATION'))
 
     def _setup_safe_master_copies(self, safe_master_copies: Sequence[Tuple[str, int, str]]):
         for address, initial_block_number, version in safe_master_copies:
@@ -170,3 +210,17 @@ class Command(BaseCommand):
                                                    'initial_block_number': initial_block_number,
                                                    'tx_block_number': initial_block_number,
                                                })
+
+    # manually setting up the environment
+    def setup_my_network(self):
+        SafeMasterCopy.objects.get_or_create(address='0xe3d4Af0e46Aa207222bC6FC11652dDF68eBF1d15',
+                                             defaults={
+                                                 'initial_block_number': 367377,
+                                                 'tx_block_number': 367377,
+                                             })
+
+        ProxyFactory.objects.get_or_create(address='0x5eAa96A55e703BD410DCdc300683a05951080218',
+                                           defaults={
+                                               'initial_block_number': 369855,
+                                               'tx_block_number': 369855,
+                                           })
