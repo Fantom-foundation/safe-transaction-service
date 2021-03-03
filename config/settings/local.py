014ad69b91b351ec647fa9e692680a336281f3c8
jason <kwon_jason@yahoo.com>
2021-03-03 21:26:59 +0300

    update allowed hosts

--- config/settings/local.py
+++ config/settings/local.py
@@ -10,11 +10,6 @@ DEBUG = env.bool('DJANGO_DEBUG', default=True)
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env('DJANGO_SECRET_KEY', default='aHdCBMHXuxIxEhfRGFRp7Cp3N9CqEZEEAvwZVlBCazKExkEnzvVs4bYWC8Qqh9lg')
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = [
-    "localhost",
-    "0.0.0.0",
-    "127.0.0.1",
-]
 
 # CACHES
 # ------------------------------------------------------------------------------
